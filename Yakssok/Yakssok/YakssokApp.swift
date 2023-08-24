f72ec247bafc4ab19026136ad2a454122048be45
seunggyun-jeong <jskyun98@gmail.com>
2023-08-24 23:08:40 +0900

    [ADD] Add ColorSet

---
+++ Yakssok/Yakssok/YakssokApp.swift
@@ -0,0 +1,17 @@
+//
+//  YakssokApp.swift
+//  Yakssok
+//
+//  Created by 정승균 on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct YakssokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
