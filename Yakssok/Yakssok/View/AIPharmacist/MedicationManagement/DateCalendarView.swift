54f1321ccd1711241fe4acee94ddd5f96e9bf70f
seunggyun-jeong <jskyun98@gmail.com>
2023-08-26 00:02:50 +0900

    [FEAT] 달력뷰 구현

---
+++ Yakssok/Yakssok/View/AIPharmacist/MedicationManagement/DateCalendarView.swift
@@ -0,0 +1,83 @@
+//
+//  DateCalendarView.swift
+//  Yakssok
+//
+//  Created by 정승균 on 2023/08/25.
+//
+
+import SwiftUI
+
+struct DateCalendarView: View {
+    @State var today: Int = 0
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            header
+                .padding(.leading, 24)
+            
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        ForEach(getListOfMonth(), id: \.self) { date in
+                            let day = Calendar.current.component(.day, from: date)
+                            
+                            DateComponent(date: date, isDone: .random(), isToday: day==today)
+                                .id(day)
+                        }
+                    }
+                }
+                .frame(height: 53)
+                .onChange(of: today) { newValue in
+                    withAnimation {
+                        proxy.scrollTo(today, anchor: .center)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            today = getTodayDate()
+        }
+    }
+    
+    func getTodayDate() -> Int {
+        let calendar = Calendar.current
+        let now = Date.now
+        
+        return calendar.component(.day, from: now)
+    }
+    
+    func getNowMonth() -> Int {
+        let calendar = Calendar.current
+        let now = Date.now
+        
+        return calendar.component(.month, from: now)
+    }
+    
+    func getListOfMonth() -> [Date] {
+        let calendar = Calendar.current
+        
+        let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: .now))!
+        
+        let components = (
+            0..<calendar.range(of: .day, in: .month, for: startDate)!.count)
+            .map {
+                calendar.date(byAdding: .day, value: $0, to: startDate)!
+            }
+        
+        return components
+    }
+}
+
+extension DateCalendarView {
+    private var header: some View {
+        Text("\(getNowMonth())월 기록")
+            .font(.body1SemiBold)
+            .foregroundColor(.theme.gray.black)
+    }
+}
+
+struct DateCalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateCalendarView()
+    }
+}
--- Yakssok/Yakssok/View/AIPharmacist/MedicationManagement/MainView.swift
+++ Yakssok/Yakssok/View/AIPharmacist/MedicationManagement/MainView.swift
@@ -9,7 +9,67 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            header
+                .padding(.horizontal, 24)
+                .padding(.bottom, 32)
+            
+            news
+                .padding(.horizontal, 24)
+                .padding(.bottom, 26)
+            
+            DateCalendarView()
+        }
+    }
+}
+
+extension MainView {
+    private var header: some View {
+        HStack {
+            Image("")
+                .frame(height: 29)
+            
+            Spacer()
+            
+            Button {
+                // 약 추가하기
+            } label: {
+                HStack {
+                    Image(systemName: "plus")
+                        .font(.custom("SF Pro", size: 12))
+                        .bold()
+                    
+                    Text("약 추가하기")
+                        .font(.caption1SemiBold)
+                }
+                .foregroundColor(.theme.primary.main0)
+                .frame(width: 89, height: 28)
+                .background(Color.theme.primary.main40)
+                .cornerRadius(8)
+            }
+        }
+    }
+    
+    private var news: some View {
+        HStack {
+            Text("뉴스")
+                .font(.caption1SemiBold)
+                .padding(.leading, 12)
+            
+            Text("대웅제약 \"엔블로, 중동 진출...사우디아라비아에 품목허가신청\"")
+                .font(.caption1)
+                .padding(.trailing, 15)
+        }
+        .lineLimit(1)
+        .padding(.vertical, 12)
+        .foregroundColor(.theme.gray.black)
+        .frame(maxWidth: .infinity)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.theme.gray.gray5, lineWidth: 1)
+        )
+        .background(Color.theme.gray.gray6)
+
     }
 }
 
