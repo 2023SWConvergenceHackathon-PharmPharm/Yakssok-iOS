ed6a244d73ac84d0db3a8f2145423f51eedc8a25
psangwon62 <psangwon62@gmail.com>
2023-08-27 03:38:17 +0900

    [FEAT] 검색 안되는 의약품 위해 url 수정

--- Yakssok/Yakssok/View/TestView.swift
+++ Yakssok/Yakssok/View/TestView.swift
@@ -1,28 +1,35 @@
-////
-////  TestView.swift
-////  Yakssok
-////
-////  Created by 박상원 on 2023/08/27.
-////
 //
-//import SwiftUI
+//  TestView.swift
+//  Yakssok
 //
-//struct TestView: View {
-//    @State var isShow: Bool = false
-//    var body: some View {
-//        Button {
-//            isShow = true
-//        } label: {
-//            Text("GO")
+//  Created by 박상원 on 2023/08/27.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    @State var isShow: Bool = false
+    var body: some View {
+        Button {
+            isShow = true
+        } label: {
+            Text("GO")
+        }
+        .sheet(isPresented: $isShow) {
+            MedicineDetailModalView(isShow: $isShow, medName: "졸피뎀")
+        }
+//        NavigationView {
+//            NavigationLink {
+//                DURAlertView()
+//            } label: {
+//                Text("GOGOOGGOGO")
+//            }
 //        }
-//        .sheet(isPresented: $isShow) {
-//            MedicineDetailModalView(isShow: $isShow, medName: "라벨라")
-//        }
-//    }
-//}
-//
-//struct TestView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TestView()
-//    }
-//}
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
